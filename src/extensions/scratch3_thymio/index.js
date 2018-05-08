abd05ef9ded433d2d424f0085abf55fae6d43fd4
Pierre Rouanet <pierre.rouanet@gmail.com>
2018-05-08 10:35:07 +0200

    Add license header.

--- src/extensions/scratch3_thymio/index.js
+++ src/extensions/scratch3_thymio/index.js
@@ -1,3 +1,21 @@
+/**
+* Thymio exension for Scratch 3.0
+* v 1.0 for internal use
+* Created by Pollen Robotics on May 7, 2018
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published
+* by the Free Software Foundation, version 3 of the License.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 const log = require('../../util/log');
 
 const aeslString = require('./aesl');
